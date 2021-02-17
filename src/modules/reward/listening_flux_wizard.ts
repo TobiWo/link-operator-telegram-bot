0af1f68989feff88a8414eeee9c559f1b6511e01
Tobias Wohland <Tobias.Wohland@t-systems.com>
2021-02-17 16:59:14 +0100

    add wizard.json, add string-constants from wizard.json, update for new address_info model

--- src/modules/reward/listening_flux_wizard.ts
+++ src/modules/reward/listening_flux_wizard.ts
@@ -1,4 +1,5 @@
 import * as FluxAggregator from '../../../artifacts/FluxAggregator.json';
+import * as wizardText from '../../../resources/wizard.json';
 import { Scenes, Composer } from 'telegraf';
 import { providers, Contract, BigNumber, utils } from 'ethers';
 import { AddressInfo } from '../../interface/address_info';
@@ -18,67 +19,68 @@ export class FluxFeedRewardWizard {
 
   getWizard(): Scenes.WizardScene<Scenes.WizardContext> {
     const stepHandler = new Composer<Scenes.WizardContext>();
-    stepHandler.command('listenOnRewardChanges', async (ctx) => {
+    stepHandler.command(wizardText.flux_feed_wizard.commands.long.start_listening, async (ctx) => {
       this.feedRewardListeningHandler(ctx);
     });
-    stepHandler.command('lrc', async (ctx) => {
+    stepHandler.command(wizardText.flux_feed_wizard.commands.short.start_listening, async (ctx) => {
       this.feedRewardListeningHandler(ctx);
     });
-    stepHandler.command('stopListening', async (ctx) => {
+    stepHandler.command(wizardText.flux_feed_wizard.commands.long.stop_listening, async (ctx) => {
       this.stopFeedRewardListeningHandler(ctx);
     });
-    stepHandler.command('sl', async (ctx) => {
+    stepHandler.command(wizardText.flux_feed_wizard.commands.short.stop_listening, async (ctx) => {
       this.stopFeedRewardListeningHandler(ctx);
     });
-    stepHandler.command('averageFluxReward', async (ctx) => {
+    stepHandler.command(wizardText.flux_feed_wizard.commands.long.average_reward, async (ctx) => {
       this.averageFeedRewardAmountHandler(ctx);
     });
-    stepHandler.command('avr', async (ctx) => {
+    stepHandler.command(wizardText.flux_feed_wizard.commands.short.average_reward, async (ctx) => {
       this.averageFeedRewardAmountHandler(ctx);
     });
-    stepHandler.command('leave', async (ctx) => {
-      await ctx.reply('Leaving module!');
+    stepHandler.command(wizardText.flux_feed_wizard.commands.long.leave, async (ctx) => {
+      await ctx.reply(wizardText.flux_feed_wizard.replies.leaving);
       return ctx.scene.leave();
     });
     stepHandler.help(async (ctx) => {
-      await ctx.reply(
-        'Available commands:\n/listenOnRewardChanges (/lrc) - listen to reward changes on every flux feed\n\
-/stopListening (/sl) - stops listening on reward changes\n\
-/averageFluxReward (/avr) - prints the current average feed-reward\n\
-/leave - leaves the current module'
-      );
+      await ctx.reply(wizardText.flux_feed_wizard.replies.help);
     });
-    const rewardWizard = new Scenes.WizardScene('flux-feed-wizard', stepHandler);
+    const rewardWizard = new Scenes.WizardScene(wizardText.flux_feed_wizard.name, stepHandler);
     return rewardWizard;
   }
 
   private async feedRewardListeningHandler(ctx: Scenes.WizardContext): Promise<void> {
-    for (const address of this.currentFeedStatus.keys()) {
-      const feedStatus: FluxFeedStatus | undefined = this.currentFeedStatus.get(address);
+    for (const name of this.currentFeedStatus.keys()) {
+      const feedStatus: FluxFeedStatus | undefined = this.currentFeedStatus.get(name);
       if (feedStatus) {
         if (feedStatus.contract.listeners(ROUND_DETAILS_UPDATED_NAME).length != 0) {
-          await ctx.reply('You already listening on flux contracts for reward changes!');
+          await ctx.reply(wizardText.flux_feed_wizard.replies.already_listening);
           return;
         }
         feedStatus.contract.on(ROUND_DETAILS_UPDATED_NAME, async (paymentAmount) => {
-          await ctx.reply(`New feed-reward on ${address}: ${this.getLinkValueWithTwoDecimals(paymentAmount)} LINK`);
-          this.updateCurrentFeedReward(address, paymentAmount);
+          await ctx.reply(
+            `${wizardText.flux_feed_wizard.replies.new_feed_reward} ${name}: ${this.getLinkValueWithTwoDecimals(
+              paymentAmount
+            )} LINK`
+          );
+          this.updateCurrentFeedReward(name, paymentAmount);
         });
       }
     }
-    await ctx.reply('Started listening on all flux-contracts!');
+    await ctx.reply(wizardText.flux_feed_wizard.replies.started_listening);
   }
 
   private async stopFeedRewardListeningHandler(ctx: Scenes.WizardContext): Promise<void> {
     for (const feedStatus of this.currentFeedStatus.values()) {
       feedStatus.contract.removeAllListeners(ROUND_DETAILS_UPDATED_NAME);
     }
-    await ctx.reply('Stopped listening on all flux-contracts!');
+    await ctx.reply(wizardText.flux_feed_wizard.replies.stopped_listening);
   }
 
   private async averageFeedRewardAmountHandler(ctx: Scenes.WizardContext): Promise<void> {
     await ctx.reply(
-      `Current average feed-reward: ${this.getLinkValueWithTwoDecimals(this.getAverageFeedRewardAmount())} LINK`
+      `${wizardText.flux_feed_wizard.replies.average_feed_reward} ${this.getLinkValueWithTwoDecimals(
+        this.getAverageFeedRewardAmount()
+      )} LINK`
     );
   }
 
@@ -90,18 +92,18 @@ export class FluxFeedRewardWizard {
     return totalRewards.div(BigNumber.from(this.currentFeedStatus.size));
   }
 
-  private updateCurrentFeedReward(address: string, newReward: BigNumber): void {
-    const fluxFeedStatus: FluxFeedStatus | undefined = this.currentFeedStatus.get(address);
+  private updateCurrentFeedReward(name: string, newReward: BigNumber): void {
+    const fluxFeedStatus: FluxFeedStatus | undefined = this.currentFeedStatus.get(name);
     if (fluxFeedStatus && !fluxFeedStatus.currentReward.eq(newReward)) {
       fluxFeedStatus.currentReward = newReward;
-      this.currentFeedStatus.set(address, fluxFeedStatus);
+      this.currentFeedStatus.set(name, fluxFeedStatus);
     }
   }
 
   private createFluxContracts(): void {
-    for (const address of this.addressYaml.flux.contracts) {
-      const contract: Contract = new Contract(address, FluxAggregator.abi, this.provider);
-      this.currentFeedStatus.set(address, { contract, currentReward: BigNumber.from(0) });
+    for (const contractInfo of this.addressYaml.flux.contracts) {
+      const contract: Contract = new Contract(contractInfo.address, FluxAggregator.abi, this.provider);
+      this.currentFeedStatus.set(contractInfo.name, { contract, currentReward: BigNumber.from(0) });
     }
   }
 
