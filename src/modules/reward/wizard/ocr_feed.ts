28b447b7e40717dbcea31a54b681ac8b29fc8c4a
Tobias Wohland <Tobias.Wohland@t-systems.com>
2021-03-06 13:53:06 +0100

    add average reward step to wizard

--- src/modules/reward/wizard/ocr_feed.ts
+++ src/modules/reward/wizard/ocr_feed.ts
@@ -1,15 +1,16 @@
 import * as wizardText from '../../../../resources/wizard.json';
+import { BigNumber, providers, utils } from 'ethers';
 import { BillingSet, FeedRewardStatus } from '../../../model/feed_reward_status';
-import { Composer, Context, Scenes } from 'telegraf';
-import { OcrFeedRewardService, WEI_PER_GWEI } from '../service/ocr_feed';
+import { Composer, Context, Markup, Scenes } from 'telegraf';
 import { AddressInfo } from '../../../model/address_info';
 import { FeedWizard } from './feed';
 import { Helper } from '../../../helper/help';
+import { OcrFeedRewardService } from '../service/ocr_feed';
 import { Replier } from '../../../general_replier';
 import { TrxInfo } from '../../../model/trx_info';
-import { providers } from 'ethers';
 
 const BILLING_SET_NAME: string = 'BillingSet';
+const GAS_USAGE: BigNumber = BigNumber.from(231255);
 
 export class OcrFeedRewardWizard extends FeedWizard<BillingSet> {
   private feedService: OcrFeedRewardService;
@@ -29,12 +30,81 @@ export class OcrFeedRewardWizard extends FeedWizard<BillingSet> {
     const ocrFeedWizard = new Scenes.WizardScene(
       wizardText.ocr_feed_wizard.name,
       wizardMainMenu,
-      this.getStartListeningConfirmationStep()
+      this.getStartListeningConfirmationStep(),
+      this.getAverageTransmitterRewardStep()
     );
     return ocrFeedWizard;
   }
 
-  getStartListeningConfirmationStep(): Composer<Scenes.WizardContext<Scenes.WizardSessionData>> {
+  private getAverageTransmitterRewardStep(): Composer<Scenes.WizardContext<Scenes.WizardSessionData>> {
+    const stepHandler = new Composer<Scenes.WizardContext>();
+    stepHandler.action(wizardText.ocr_feed_wizard.requests.gasPriceAction._50, async (ctx) => {
+      await this.sendAverageTransmitterRewardReply(ctx, '50');
+      await this.sendAverageObservationRewardReply(ctx);
+      return ctx.wizard.selectStep(0);
+    });
+    stepHandler.action(wizardText.ocr_feed_wizard.requests.gasPriceAction._100, async (ctx) => {
+      await this.sendAverageTransmitterRewardReply(ctx, '100');
+      await this.sendAverageObservationRewardReply(ctx);
+      return ctx.wizard.selectStep(0);
+    });
+    stepHandler.action(wizardText.ocr_feed_wizard.requests.gasPriceAction._200, async (ctx) => {
+      await this.sendAverageTransmitterRewardReply(ctx, '200');
+      await this.sendAverageObservationRewardReply(ctx);
+      return ctx.wizard.selectStep(0);
+    });
+    stepHandler.action(wizardText.ocr_feed_wizard.requests.gasPriceAction._400, async (ctx) => {
+      await this.sendAverageTransmitterRewardReply(ctx, '400');
+      await this.sendAverageObservationRewardReply(ctx);
+      return ctx.wizard.selectStep(0);
+    });
+    stepHandler.action(wizardText.ocr_feed_wizard.requests.gasPriceAction._700, async (ctx) => {
+      await this.sendAverageTransmitterRewardReply(ctx, '700');
+      await this.sendAverageObservationRewardReply(ctx);
+      return ctx.wizard.selectStep(0);
+    });
+    stepHandler.action(wizardText.ocr_feed_wizard.requests.gasPriceAction.current, async (ctx) => {
+      await this.sendAverageTransmitterRewardReply(ctx, '1');
+      await this.sendAverageObservationRewardReply(ctx);
+      return ctx.wizard.selectStep(0);
+    });
+    return stepHandler;
+  }
+
+  private async sendAverageObservationRewardReply(ctx: Scenes.WizardContext): Promise<void> {
+    let totalObservationReward: BigNumber = BigNumber.from(0);
+    for (const feedStatus of this.currentFeedStatus.values()) {
+      totalObservationReward = totalObservationReward.add(feedStatus.rewardData.linkWeiPerObservation);
+    }
+    await ctx.reply(
+      wizardText.ocr_feed_wizard.replies.average_observation_feed_reward.format(
+        Helper.getLinkValueWithDefinedDecimals(totalObservationReward.div(this.currentFeedStatus.size), 3)
+      )
+    );
+  }
+
+  private async sendAverageTransmitterRewardReply(ctx: Scenes.WizardContext, gasPriceInGwei: string): Promise<void> {
+    await ctx.reply(
+      wizardText.ocr_feed_wizard.replies.average_transmitter_feed_reward.format(
+        Helper.getLinkValueWithDefinedDecimals(
+          this.getAverageTransmitterReward(utils.parseUnits(gasPriceInGwei, 'gwei')),
+          3
+        )
+      )
+    );
+  }
+
+  private getAverageTransmitterReward(gasPrice: BigNumber): BigNumber {
+    let totalTransmitterRewards: BigNumber = BigNumber.from(0);
+    for (const feedStatus of this.currentFeedStatus.values()) {
+      totalTransmitterRewards = totalTransmitterRewards.add(
+        this.feedService._getNewTransmitterReward(feedStatus.rewardData, gasPrice, GAS_USAGE)
+      );
+    }
+    return totalTransmitterRewards.div(this.currentFeedStatus.size);
+  }
+
+  private getStartListeningConfirmationStep(): Composer<Scenes.WizardContext<Scenes.WizardSessionData>> {
     const stepHandler = new Composer<Scenes.WizardContext>();
     stepHandler.action(wizardText.listening_service.action.start, async (ctx) => {
       this.feedRewardListeningStep(ctx);
@@ -61,6 +131,12 @@ export class OcrFeedRewardWizard extends FeedWizard<BillingSet> {
     mainMenu.command(wizardText.commands.short.stop_listening, async (ctx) => {
       this.stopFeedRewardListeningStep(ctx);
     });
+    mainMenu.command(wizardText.commands.long.average_reward, async (ctx) => {
+      this.gasPriceRequest(2, ctx);
+    });
+    mainMenu.command(wizardText.commands.short.average_reward, async (ctx) => {
+      this.gasPriceRequest(2, ctx);
+    });
     mainMenu.command(wizardText.commands.long.leave, async (ctx) => {
       await ctx.reply(wizardText.general_replies.leaving);
       await Replier.replyBotMainMenu(ctx);
@@ -70,6 +146,42 @@ export class OcrFeedRewardWizard extends FeedWizard<BillingSet> {
       await ctx.replyWithMarkdownV2(wizardText.ocr_feed_wizard.replies.help);
     });
     return mainMenu;
+  }
+
+  async gasPriceRequest(
+    selectStep: number,
+    ctx: Scenes.WizardContext
+  ): Promise<Scenes.WizardContextWizard<Scenes.WizardContext<Scenes.WizardSessionData>>> {
+    await ctx.replyWithMarkdownV2(
+      '*Please select the gas price for which you want to receive the average transmitter reward*',
+      Markup.inlineKeyboard([
+        Markup.button.callback(
+          wizardText.ocr_feed_wizard.requests.gasPriceAction._50,
+          wizardText.ocr_feed_wizard.requests.gasPriceAction._50
+        ),
+        Markup.button.callback(
+          wizardText.ocr_feed_wizard.requests.gasPriceAction._100,
+          wizardText.ocr_feed_wizard.requests.gasPriceAction._100
+        ),
+        Markup.button.callback(
+          wizardText.ocr_feed_wizard.requests.gasPriceAction._200,
+          wizardText.ocr_feed_wizard.requests.gasPriceAction._200
+        ),
+        Markup.button.callback(
+          wizardText.ocr_feed_wizard.requests.gasPriceAction._400,
+          wizardText.ocr_feed_wizard.requests.gasPriceAction._400
+        ),
+        Markup.button.callback(
+          wizardText.ocr_feed_wizard.requests.gasPriceAction._700,
+          wizardText.ocr_feed_wizard.requests.gasPriceAction._700
+        ),
+        Markup.button.callback(
+          wizardText.ocr_feed_wizard.requests.gasPriceAction.current,
+          wizardText.ocr_feed_wizard.requests.gasPriceAction.current
+        ),
+      ])
+    );
+    return ctx.wizard.selectStep(selectStep);
   }
 
   private async feedRewardListeningStep(ctx: Scenes.WizardContext): Promise<void> {
@@ -117,7 +229,7 @@ export class OcrFeedRewardWizard extends FeedWizard<BillingSet> {
             ),
             2
           ),
-          trxInfo.transactionResponse.gasPrice.div(WEI_PER_GWEI).toString(),
+          trxInfo.transactionResponse.gasPrice.div(utils.parseUnits('1', 'gwei')).toString(),
           this.feedService._getReplyForChangedTransmitterValues(this.getFeedStatus(feedName), billingSet)
         )
       );
