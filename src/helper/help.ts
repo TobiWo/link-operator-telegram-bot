c553355df1093e76f282eb83f7d41369e65fd1bc
Tobias Wohland <Tobias.Wohland@t-systems.com>
2021-03-07 23:53:50 +0100

    refactor method name

--- src/helper/help.ts
+++ src/helper/help.ts
@@ -1,7 +1,7 @@
 import { utils, BigNumber } from 'ethers';
 
 export class Helper {
-  static getLinkValueWithDefinedDecimals(linkInWei: BigNumber, decimals: number): string {
+  static parseLinkWeiToLink(linkInWei: BigNumber, decimals: number): string {
     const valueString: string = utils.formatEther(linkInWei);
     return valueString.slice(0, valueString.indexOf('.') + decimals + 1);
   }
