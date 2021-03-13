5196e41bee7193cd042145ff2505a4df8be9c3fc
Tobias Wohland <Tobias.Wohland@t-systems.com>
2021-03-13 13:41:38 +0100

    change logger to only log on console

--- src/logger.ts
+++ src/logger.ts
@@ -1,19 +1,14 @@
 import { createLogger, transports, format } from 'winston';
 
-const cwd = process.cwd().split('src')[0];
-
 export const logger = createLogger({
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     format.printf((info) => `${info.timestamp} chainlink-bot ${info.level.toUpperCase()}: ${info.message}`)
   ),
   transports: [
-    new transports.File({
-      filename: `${cwd}/logs/chainlink-bot.log`,
-      level: 'info',
-      maxFiles: 10,
-      maxsize: 10485760,
-    }),
+    new transports.Console({ level: 'error' }),
     new transports.Console({ level: 'debug' }),
+    new transports.Console({ level: 'info' }),
+    new transports.Console({ level: 'warn' }),
   ],
 });
